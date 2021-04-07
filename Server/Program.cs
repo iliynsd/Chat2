4e4ab056e19807d9a577b68d3cfd9dbe7f5d2e9b
AnSt <starininandrey@gmail.com>
2021-04-07 20:48:43 +0300

    Небольшие правки

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,37 +5,36 @@ using System.Text;
 
 namespace Server
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            IPAddress ip = IPAddress.Parse("127.0.0.1");
-            int port = 8005;
-            IPEndPoint ip_server = new IPEndPoint(ip, port);
+            var ip = IPAddress.Parse("127.0.0.1");
+            const int PORT = 8005;
+            var ipServer = new IPEndPoint(ip, PORT);
 
-            Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            listen.Bind(ip_server);
+            var listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listen.Bind(ipServer);
             listen.Listen(10);
 
             while (true)
             {
-                Socket client = listen.Accept();
+                var client = listen.Accept();
 
-                StringBuilder message = new StringBuilder();
-                int bytes = 0;
-                byte[] data = new byte[256];
+                var message = new StringBuilder();
+                var data = new byte[256];
 
                 do
                 {
-                    bytes = client.Receive(data);
+                    var bytes = client.Receive(data);
                     message.Append(Encoding.Unicode.GetString(data, 0, bytes));
                 } while (client.Available > 0);
                 
                 Console.WriteLine($"{DateTime.Now:u}: {message}");
 
-                string msg = "Сообщение получено";
-                byte[] data_send = Encoding.Unicode.GetBytes(msg);
-                client.Send(data_send);
+                const string MSG = "Сообщение получено";
+                var dataSend = Encoding.Unicode.GetBytes(MSG);
+                client.Send(dataSend);
                 
                 client.Shutdown(SocketShutdown.Both);
                 client.Close();
