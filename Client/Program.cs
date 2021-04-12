b6e670afe92ec6ce41b287bd815a2aee1c72808d
AnSt <starininandrey@gmail.com>
2021-04-12 20:23:20 +0300

    Init

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,39 +1,10 @@
-﻿using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-
-namespace Client
+﻿namespace Client
 {
     internal static class Program
     {
         private static void Main()
         {
-            var ip = IPAddress.Parse("127.0.0.1");
-            const int PORT = 8005;
-            var ipServer = new IPEndPoint(ip, PORT);
-
-            var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            server.Connect(ipServer);
-
-            while (true)
-            {
-                var message = Console.ReadLine();
-                var data = Encoding.Unicode.GetBytes(message);
-                server.Send(data);
             
-                var msg = new StringBuilder();
-                var dataSend = new byte[256];
-                do
-                {
-                    var bytes = server.Receive(data);
-                    msg.Append(Encoding.Unicode.GetString(dataSend, 0, bytes));
-                } while (server.Available > 0);
-                Console.WriteLine($"{DateTime.Now:u}: {msg}");
-            }
-            
-            server.Shutdown(SocketShutdown.Both);
-            server.Close();
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,62 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Server
+﻿namespace Server
 {
     internal static class Program
     {
         private static void Main()
         {
-            var ip = IPAddress.Parse("192.168.0.53");
-            const int PORT = 8005;
-            var ipServer = new IPEndPoint(ip, PORT);
-
-            var listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            listen.Bind(ipServer);
-            listen.Listen(10);
-
-            int count = 0;
-            var clients = new Dictionary<int, Socket>();
-
-            while (true)
-            {
-                var client = listen.Accept();
-                count++;
-                clients.Add(count, client);
-                var count1 = count;
-                var task = Task.Run(() => NewClient(client, count1, ref clients));
-            }
-        }
-
-        static void NewClient(Socket client, int id, ref Dictionary<int, Socket> clients)
-        {
-            var message = new StringBuilder();
-            var data = new byte[256];
-            while (true)
-            {
-                message.Clear();
-                do
-                {
-                    var bytes = client.Receive(data);
-                    message.Append(Encoding.Unicode.GetString(data, 0, bytes));
-                } while (client.Available > 0);
-
-                var temp = $"#{id} {DateTime.Now:u}: {message}";
-                Console.WriteLine(temp);
-                
-                var dataSend = Encoding.Unicode.GetBytes(temp);
-
-                foreach (var item in clients)
-                {
-                    item.Value.Send(dataSend);
-                }
-            }
-            client.Shutdown(SocketShutdown.Both);
-            client.Close();
+            
         }
     }
 }
