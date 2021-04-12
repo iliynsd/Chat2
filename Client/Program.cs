de7687babc2c6f6be2558180dd3c5c47fd0dbe49
AnSt <starininandrey@gmail.com>
2021-04-12 21:50:53 +0300

    Update

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Client
+﻿using TCPLibrary;
+
+namespace Client
 {
     internal static class Program
     {
         private static void Main()
         {
-            
+            var client = new TCPClient();
+            client.Connect();
+            client.SendMessage("Hello");
+            client.Close();
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Server
+﻿using System;
+using TCPLibrary;
+
+namespace Server
 {
     internal static class Program
     {
         private static void Main()
         {
-            
+            var server = new TCPServer();
+            server.Start();
+            while (true)
+            {
+                var client = server.NewClient();
+                Console.WriteLine(client.GetMessage());
+                client.Close();
+            }
         }
     }
 }
