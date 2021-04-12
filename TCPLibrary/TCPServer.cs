351a7d7756b84118c203ce3b60ba02c53e8be612
AnSt <starininandrey@gmail.com>
2021-04-12 21:39:45 +0300

    Init TCPLibrary

---
+++ TCPLibrary/TCPServer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TCPLibrary
+{
+    public class TCPServer : TCP
+    {
+        public TCPServer() : base() {}
+        public TCPServer(string ipAddress, int port) : base(ipAddress, port) {}
+
+        public bool Start()
+        {
+            try
+            {
+                _socket.Bind(_ipServer);
+                _socket.Listen(10);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public TCPClient NewClient()
+        {
+            try
+            {
+                var client = new TCPClient(_socket.Accept());
+                return client;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ошибка соединения с клиентом");
+            }
+        }
+    }
+}
