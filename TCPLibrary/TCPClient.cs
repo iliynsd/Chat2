72679a4bef2b2da626857cf2ea9644e8a8fc7c7b
AnSt <starininandrey@gmail.com>
2021-04-12 21:40:17 +0300

    Init TCPLibrary

---
+++ TCPLibrary/TCPClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace TCPLibrary
+{
+    public class TCPClient : TCP
+    {
+        public TCPClient() : base() {}
+
+        public TCPClient(string ipAddress, int port) : base(ipAddress, port) {}
+
+        public TCPClient(Socket socket)
+        {
+            _socket = socket ?? throw new Exception("Ошибка создания клиента");
+        }
+        
+        public bool Connect()
+                {
+                    try
+                    {
+                        _socket.Connect(_ipServer);
+                    }
+                    catch (SocketException)
+                    {
+                        return false;
+                    }
+        
+                    return true;
+                }
+    }
+}
